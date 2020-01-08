45935744a228cbaa7679ae63bf75d050f36b4420
Yura <diakonov54@gmail.com>
2020-01-08 21:03:10 +0500

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+// alert("Привет, долбаеб!")
+// let answer = confirm("Ты опять на связь выходишь мудила?!");
+// console.log(answer);
+
+// let answer = prompt("18 есть?", "Да");
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+    
+// incr++;
+// decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+// console.log(5%2);
+// console.log("2" == 2);
+
+let isChecked = true,
+    isClose = true;
+    console.log(isChecked || isClose);
